96ed1de537de9645a29c2fe41f914e6e53d75133
stdiodh <playlistdh@gmail.com>
2025-11-08 16:59:10 +0900

    test(calculator): CalculatorService 단위 테스트 추가
    - CalculatorService가 도메인 객체를 정상적으로 호출하여 결과를 반환하는지 검증하는 테스트 코드 작성
    - 입력값("1,2:3")에 대한 예상 결과(6) 확인 테스트 케이스 추가

--- src/test/java/woowacourse_precoruse/java_open_mission_8/calculator/service/CalculatorServiceTest.java
+++ src/test/java/woowacourse_precoruse/java_open_mission_8/calculator/service/CalculatorServiceTest.java
@@ -1,4 +1,20 @@
 package woowacourse_precoruse.java_open_mission_8.calculator.service;
 
+import org.junit.jupiter.api.Test;
+import woowacourse_precoruse.java_open_mission_8.calculator.dto.CalculatorRequest;
+import woowacourse_precoruse.java_open_mission_8.calculator.dto.CalculatorResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculatorServiceTest {
+    private final CalculatorService calculatorService = new CalculatorService();
+
+    @Test
+    void 서비스는_도메인을_호출하여_올바른_계산_결과를_반환한다() {
+        CalculatorRequest request = new CalculatorRequest("1,2:3");
+
+        CalculatorResponse response = calculatorService.calculate(request);
+
+        assertThat(response.result()).isEqualTo(6);
+    }
 }
