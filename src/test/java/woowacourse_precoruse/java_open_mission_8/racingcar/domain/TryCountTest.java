00c3d4f006fab54016e22730c5f7af40a07fe69a
stdiodh <playlistdh@gmail.com>
2025-11-10 11:09:53 +0900

    feat(domain): 시도 횟수(TryCount) 객체 및 테스트 추가함 레이싱카 게임의 시도 횟수 원시값 포장 객체인 TryCount를 정의하고 이에 대한 단위 테스트를 추가함.
    TryCount record 추가: 시도 횟수(int)를 포장함. 1 미만의 값이 들어올 경우 생성 시점에서 IllegalArgumentException을 발생시켜 유효성을 검증함.
    TryCountTest 추가: 유효한 값(1 이상)과 무효한 값(0 이하)에 대해 객체가 정상적으로 생성되거나 예외를 발생시키는지 검증함.

---
+++ src/test/java/woowacourse_precoruse/java_open_mission_8/racingcar/domain/TryCountTest.java
@@ -0,0 +1,33 @@
+package woowacourse_precoruse.java_open_mission_8.racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class TryCountTest {
+    @Test
+    void 유효한_시도_횟수일_때_정상적으로_생성한다() {
+        assertThatNoException().isThrownBy(() -> new TryCount(1));
+        assertThatNoException().isThrownBy(() -> new TryCount(5));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -1000})
+    void 시도_횟수가_1_이상이지_않으면_예외를_발생한다(Integer input) {
+        assertThatThrownBy(() -> new TryCount(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 이상");
+    }
+
+    @Test
+    void value_메서드로_정확한_값을_반환한다() {
+        int expectedValue = 10;
+        TryCount tryCount = new TryCount(expectedValue);
+
+        assertThat(tryCount.value()).isEqualTo(expectedValue);
+    }
+}
