397686151c89874c0cdc02bc4167a1c4403dfb01
stdiodh <playlistdh@gmail.com>
2025-11-12 10:22:40 +0900

    feat(lotto): 로또(Lotto) 도메인 객체 구현
    기존 LottoNumber를 사용하여 로또 한 장을 의미하는 Lotto 도메인 객체를 구현함.
    Lotto 객체 생성 시 번호 개수와 중복 여부를 검증함.
    
    - Lotto.java: List<LottoNumber>를 사용하여 도메인 구현
    - Lotto.java: 번호 6개 검증 및 중복 검증 로직 추가
    - Lotto.java: 생성 시 번호가 정렬되도록 구현
    - LottoTest.java: Lotto 도메인 유효성 검사 테스트 코드 작성

--- src/main/java/woowacourse_precoruse/java_open_mission_8/lotto/domain/Lotto.java
+++ src/main/java/woowacourse_precoruse/java_open_mission_8/lotto/domain/Lotto.java
@@ -1,4 +1,39 @@
 package woowacourse_precoruse.java_open_mission_8.lotto.domain;
 
+import java.util.List;
+
 public class Lotto {
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    private final List<LottoNumber> numbers;
+
+    public Lotto(List<LottoNumber> numbers) {
+        validate(numbers);
+
+        this.numbers = numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    private void validate(List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<LottoNumber> numbers) {
+        if (numbers == null || numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + LOTTO_NUMBER_COUNT + "개여야 합니다.");
+        }
+    }
+
+    private void validateDuplicate(List<LottoNumber> numbers) {
+        long uniqueCount = numbers.stream().distinct().count();
+        if (uniqueCount != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return numbers;
+    }
 }
