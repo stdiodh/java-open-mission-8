6a35562c0c41781ff9fe198f641b30347c0e12d2
stdiodh <playlistdh@gmail.com>
2025-11-13 16:33:36 +0900

    feat(config): SpringDoc(Swagger) 추가 및 버전 호환성 해결
    WebFlux 환경에 SpringDoc(Swagger UI)를 적용하여 API 명세서를 자동 생성함.
    
    - `build.gradle`에 `springdoc-openapi-starter-webflux-ui` 의존성 추가
    - `SwaggerConfig.java`를 통해 `OpenAPI` 빈을 등록하여 API 정보 설정
    
    `NoSuchMethodError` 버전 충돌 문제를 해결하기 위해
    Spring Boot 버전을 3.3.5로, SpringDoc 버전을 2.3.0으로 조정함.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.7"
+	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation ("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
 	implementation("com.github.woowacourse-projects:mission-utils:1.2.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
---
+++ src/main/java/woowacourse_precoruse/java_open_mission_8/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+// 예: src/main/java/.../config/SwaggerConfig.java
+
+package woowacourse_precoruse.java_open_mission_8.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Java 오픈 미션 8 - API 명세서")
+                .description("WebFlux 기반의 자동차 경주 및 로또 미션 API 문서입니다.")
+                .version("v1.0.0");
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"));
+    }
+}
