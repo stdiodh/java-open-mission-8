0377ec4688148617711e75d3471cceb94faacb0f
stdiodh <playlistdh@gmail.com>
2025-11-14 17:39:29 +0900

    chore(docker): docker-compose.yml의 중복된 environment 블록 제거함
    app 서비스의 'env_file' 설정이 .env 파일의 모든 변수(SERVER_URL 포함)를
    자동으로 주입하므로, 불필요하게 중복 선언된 'environment' 블록을 삭제함.

--- src/main/java/woowacourse_precoruse/java_open_mission_8/common/config/SwaggerConfig.java
+++ src/main/java/woowacourse_precoruse/java_open_mission_8/common/config/SwaggerConfig.java
@@ -5,11 +5,17 @@ package woowacourse_precoruse.java_open_mission_8.common.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private final String serverUrl;
+
+    public SwaggerConfig(@Value("${swagger.server.url}") String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
 
     @Bean
     public OpenAPI openAPI() {
@@ -18,8 +24,10 @@ public class SwaggerConfig {
                 .description("WebFlux 기반의 자동차 경주 및 로또 미션 API 문서입니다.")
                 .version("v1.0.0");
 
+        Server server = new Server().url(serverUrl);
+
         return new OpenAPI()
                 .info(info)
-                .addServersItem(new Server().url("/"));
+                .addServersItem(server);
     }
 }
